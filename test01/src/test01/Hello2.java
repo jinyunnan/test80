fdc49c2686ba13a280ea3e09f98d4759affbaf80
bit <kmintokyo@gmail.com>
2016-04-20 15:18:41 +0900

    .

--- test01/src/test01/Hello2.java
+++ test01/src/test01/Hello2.java
@@ -5,6 +5,8 @@ public class Hello2 {
   public static void main(String[] args) {
      System.out.println("Hello");
      System.out.println("okok");
+     System.out.println("nnnnnnnnnn");
+
   }
 
 }
