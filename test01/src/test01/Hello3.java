6f95f1bde0c502d3af6f0c8c5c7c6d4641d8b3d5
bit <kmintokyo@gmail.com>
2016-04-20 15:48:00 +0900

    .

---
+++ test01/src/test01/Hello3.java
@@ -0,0 +1,14 @@
+package test01;
+
+public class Hello3 {
+
+  public static void main(String[] args) {
+     System.out.println("Hello");
+     System.out.println("okok");
+     
+
+     
+
+  }
+
+}
